c6a8cd43425227325c2ccb10f0e458d2a9178d9f
omer <you@example.com>
2022-11-18 11:37:31 +0300

    completd assigment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+let names = ["Guadalupe", "Ollie", "Aki"];
+let event = "surprise";
+
+function writeCards (names, event) {
+    let thanksArr = [];
+    for (let i=0; i < names.length; i++){
+        thanksArr.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return thanksArr;
+}
+
+
+function countDown(num){
+    while(0 <= num){
+        console.log(num--);
+    }
+}
+countDown(10);
+countDown(4);
